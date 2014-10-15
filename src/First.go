ced4758d56f1c8724ee2c254bc113159c2d038de
zolachang <zola.chang@spring-trees.com>
2014-10-15 15:06:56 +0800

    Hello

--- src/First.go
+++ src/First.go
@@ -9,7 +9,7 @@ import (
 func getuser(w http.ResponseWriter, r *http.Request) {
 	params := r.URL.Query()
 	uid := params.Get(":uid")
-	fmt.Fprintf(w, "you are get userss19 %s", uid)
+	fmt.Fprintf(w, "you are get userss20 %s", uid)
 }
 
 func modifyuser(w http.ResponseWriter, r *http.Request) {
